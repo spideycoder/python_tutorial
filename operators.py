2eaca3ea4f699124590cc99c0557df097e1cf60d
Aman Jain <aman@Amans-MacBook-Air.local>
2022-01-25 22:59:07 +0530

    Mah first commit

---
+++ operators.py
@@ -0,0 +1,38 @@
+## Arithmetic operators
+
+x = 10
+
+y = 20
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+## Logical operators
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+
+## Comparison Operators
+print(" -------   Comparison Operators --------- ")
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(x == y)
+print(x != y)
+
+# Association operators
+print(" -------   Association Operators --------- ")
+l = [1, 2, 3, 4, "aman", 2.5, True, [2, 3]]
+
+print(3 in l)
+print(3 not in l)
+
+# bitwise operators
---
+++ variables.py
@@ -0,0 +1,14 @@
+a = 10  # int
+s = "aman"  # string
+f = 2.5  # float
+b = True  # False # boolean
+
+print(a)
+print(s)
+print(f)
+print(b)
+
+l = [1, 2, 3, 4, "aman", 2.5, True, [2, 3]]  # mutable
+t = (1, 2, 3, 4, "aman", 2.5, True, (2, 3))  # immutable
+
+# Dictionaries
