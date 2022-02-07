00eb975e393284074c5c185794f65f9c619e8344
Aman Jain <aman@Amans-MacBook-Air.local>
2022-02-08 00:05:00 +0530

    assignment on operator

---
+++ assignments/operators_assignment.py
@@ -0,0 +1,34 @@
+a = 2
+b = 3
+
+##Arthimetic operator
+print(a + b)
+print(a * b)
+print(a - b, a / b, a ** b, a // b, a % b)
+
+## Comparison Operator
+print(a > b, a < b, a <= b, a >= b)
+print(a == b, a != b)
+
+## Assignment operator
+
+a += 2
+print(a)
+a *= 10
+print(a)
+a /= 3
+print(a)
+a -= 3
+print(a)
+a **= 2
+print(a)
+a //= 4
+print(a)
+a %= 3
+print(a)
+
+## Membership operator
+
+a = [1, 2, 4, "aman", 4.5]
+b = (1)
+print(b in a)
